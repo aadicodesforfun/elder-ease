06e71e22df9d19fb3f1d7a8ccb99d27da6e3c99d
Aditya Tripathi <adityakumartripathi6285@gmail.com>
2025-08-30 14:58:01 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const discover = document.querySelector('.discover-more');
+const discover_btn = document.querySelector('.discover-btn');
+
+discover_btn.addEventListener('click', () => {
+    discover.style.display = "flex";
+})

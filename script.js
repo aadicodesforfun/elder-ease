f3e0a8483fd14b9e15169720ec5a0dbd8886ad50
Aditya Tripathi <adityakumartripathi6285@gmail.com>
2025-08-30 16:44:19 +0530

    Add chat page

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+// import {gsap} from "gsap";
+
 const discover = document.querySelector('.discover-more');
 const discover_btn = document.querySelector('.discover-btn');
+const hero = document.querySelector('.hero');
+const footer = document.querySelector('footer');
+
+const discover_close = document.querySelector('.discover-close');
+
+const chat_btn = document.querySelector('.chat');
+const chat_page = document.querySelector('.chat-ai');
 
 discover_btn.addEventListener('click', () => {
     discover.style.display = "flex";
+})
+
+discover_close.addEventListener('click', () => {
+    discover.style.display = "none";
+})
+
+chat_btn.addEventListener('click', () => {
+    chat_page.style.display = "flex";
+    hero.style.display = "none";
+    footer.style.display = "none";
 })
